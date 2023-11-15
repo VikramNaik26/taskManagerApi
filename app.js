7ffd3673a05cefa95e59f1fb4eb3ac0e4694ad0d
Vikram Naik <26vikramnaik@gmail.com>
2023-11-15 23:59:57 +0530

    env set up

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+// require('./db/connect')
 const express = require("express");
 const app = express();
+const connectDB = require("./db/connect");
+require("dotenv").config();
 
 const tasks = require("./routes/tasks");
 
@@ -21,6 +24,15 @@ app.use("/api/v1/tasks", tasks);
 // app.patch('/api/v1/tasks/:id') - updates a single task
 // app.delete('/api/v1/tasks/:id') - deletes a single task
 
-app.listen(port, () => {
-  console.log(`server listening on port ${port}...`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, () => {
+      console.log(`server listening on port ${port}...`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
--- db/connect.js
+++ db/connect.js
@@ -1,2 +1,27 @@
+const mongoose = require("mongoose");
+
 const connectionString =
-  "mongodb+srv://vikramnaik:<password>@taskmanagerproject.uzh4zod.mongodb.net/?retryWrites=true&w=majority";
+  ;
+
+/* mongoose
+  .connect(connectionString, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log("connected to db"))
+  .catch((err) => console.log(err));
+ */
+
+const connectDB = (url) => {
+    return mongoose
+  .connect(url, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+}
+
+module.exports = connectDB;
